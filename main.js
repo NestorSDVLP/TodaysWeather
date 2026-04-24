a1b14cd5e60e33472c8b3b4225eb06720e481dda
Nestor <nestors.dvlp@gmail.com>
2026-04-24 18:52:52 -0300

    first working version

---
+++ main.js
@@ -0,0 +1,52 @@
+/* global bootstrap: false */
+import { API_KEY } from "./config.js";
+
+(() => {
+  'use strict'
+
+  const getWeather = async () => {
+
+    try {
+
+        const response = await fetch(
+            `https://api.openweathermap.org/data/2.5/weather?q=Buenos Aires&appid=${API_KEY}&units=metric&lang=es`
+        );
+
+        if (!response.ok) {
+            throw new Error("Error en la respuesta de la API");
+            console.log("Error en la respuesta de la API");
+        }
+
+        const data = await response.json();
+        
+        console.log(data);
+
+        const now = new Date();
+        const lastUpdated = now.toLocaleString("es-AR");
+
+        document.querySelector("#cardWeather .city-name").textContent = data.name;
+
+        document.querySelector("#cardWeather .weather-icon").setAttribute("src", `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`);
+
+        document.querySelector("#cardWeather .weather-temp").textContent = data.main.temp;
+
+        document.querySelector("#cardWeather .weather-temp-min").textContent = data.main.temp_min;
+
+        document.querySelector("#cardWeather .weather-temp-max").textContent = data.main.temp_max;
+
+        document.querySelector("#cardWeather .weather-desc").textContent = data.weather[0].description;
+
+        document.querySelector("#cardWeather .last-updated").textContent = lastUpdated;
+
+    } catch (error) {
+
+        console.error(error);
+
+        document.querySelector(".card-body").innerHTML = "<p>(!) Error fetching weather data</p>";
+
+    }
+  };
+
+  getWeather();
+
+})()
